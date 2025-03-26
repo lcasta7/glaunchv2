57d362bfbfd54c0c95f7a86642fe81c8495843c7
luis <luis@casta.dev>
2025-03-25 19:42:28 -0500

    lc-glaunchv2/github-actions: added eslint

--- extension.ts
+++ extension.ts
@@ -1,60 +1,56 @@
-import GLib from 'gi://GLib';
-import Gio from 'gi://Gio';
-import GObject from 'gi://GObject';
-import Meta from 'gi://Meta';
-import Shell from 'gi://Shell';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import GLib from "gi://GLib";
+import Gio from "gi://Gio";
+import GObject from "gi://GObject";
+import Meta from "gi://Meta";
+import Shell from "gi://Shell";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 export default class GlaunchV2 extends Extension {
+  enable() {
+    //set[string]App
+    let appMap = new Map<string, Meta.Window>();
+    global.window_manager.connect("map", (_, win: Meta.WindowActor) => {
+      //we launch with this if it's available/otherwise wmAppName
+      // const gtkAppName = win.get_meta_window()?.get_gtk_application_id();
 
-	enable() {
+      const metaWindow: Meta.Window = win.get_meta_window()!;
+      const wmAppName: string = metaWindow?.get_wm_class()?.toLowerCase() ?? "";
+      if (wmAppName === "" || wmAppName === "gjs") {
+        return;
+      }
 
-		//set[string]App
-		let appMap = new Map<string, Meta.Window>();
-		global.window_manager.connect('map', (_, win: Meta.WindowActor) => {
-			//we launch with this if it's available/otherwise wmAppName
-			// const gtkAppName = win.get_meta_window()?.get_gtk_application_id();
+      if (metaWindow) {
+        appMap.set(wmAppName, metaWindow);
+      }
+    });
 
-			const metaWindow: Meta.Window = win.get_meta_window()!;
-			const wmAppName: string = metaWindow?.get_wm_class()?.toLowerCase() ?? "";
-			if (wmAppName === "" || wmAppName === "gjs") {
-				return;
-			}
+    global.window_manager.connect("destroy", (_, win: Meta.WindowActor) => {
+      const metaWindow: Meta.Window = win.get_meta_window()!;
+      const wmAppName: string = metaWindow?.get_wm_class() ?? "";
+      if (appMap.has(wmAppName)) {
+        appMap.delete(wmAppName);
+      }
+    });
 
+    let settings: Gio.Settings = this.getSettings(
+      "org.gnome.shell.extensions.glaunchv2",
+    );
 
-			if (metaWindow) {
-				appMap.set(wmAppName, metaWindow);
-			}
-		});
+    Main.wm.addKeybinding(
+      "f6",
+      settings,
+      Meta.KeyBindingFlags.NONE,
+      Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
+      () => {
+        console.log("[GlaunchV2] AppMap contents:");
+        appMap.forEach((metaWin, key) => {
+          console.log(`  Key: "${key}", Window ID: ${metaWin.get_id()}`);
+        });
+        console.log(`[GlaunchV2] Total entries: ${appMap.size}`);
+      },
+    );
+  }
 
-		global.window_manager.connect('destroy', (_, win: Meta.WindowActor) => {
-			const metaWindow: Meta.Window = win.get_meta_window()!;
-			const wmAppName: string = metaWindow?.get_wm_class() ?? "";
-			if (appMap.has(wmAppName)) {
-				appMap.delete(wmAppName);
-
-			}
-
-		});
-
-		let settings: Gio.Settings = this.getSettings('org.gnome.shell.extensions.glaunchv2');
-		Main.wm.addKeybinding(
-			'f6',
-			settings,
-			Meta.KeyBindingFlags.NONE,
-			Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
-			() => {
-				console.log('[GlaunchV2] AppMap contents:');
-				appMap.forEach((metaWin, key) => {
-					console.log(`  Key: "${key}", Window ID: ${metaWin.get_id()}`);
-				});
-				console.log(`[GlaunchV2] Total entries: ${appMap.size}`);
-			}
-		);
-	}
-
-
-	disable() {
-	}
+  disable() {}
 }
